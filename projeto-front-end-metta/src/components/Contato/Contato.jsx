ea8a19c796af6d1548babc5fcc7825a099d1c5c0
VittorioBarella <vittorio.br@hotmail.com>
2021-06-18 22:55:31 -0300

    Bugfix no Header, cor no footer e implementação do layout para o componente contato.

--- projeto-front-end-metta/src/components/Contato/Contato.jsx
+++ projeto-front-end-metta/src/components/Contato/Contato.jsx
@@ -3,9 +3,47 @@ import '../Contato/style.css';
 function Contato() {
   return (
     <>
-      <div className='container-fluid'>
-        <h4>Deixe sua opinião</h4>
-        <h1>Me diga o que achou, um crítica, conselho ou algo a melhorar.</h1>
+      <div className='container-fluid fadeIn mt-3'>
+        <h3>Me diga o que achou, uma crítica, conselho ou algo a melhorar.</h3>
+        <form>
+          <div className='form-group'>
+            <label for='exampleFormControlSelect1'>Nome:</label>
+            <input
+              className='form-control'
+              type='text'
+              placeholder='Digite seu nome'
+            ></input>
+          </div>
+          <div className='form-group'>
+            <label for='exampleFormControlInput1'>Email:</label>
+            <input
+              type='email'
+              className='form-control'
+              id='exampleFormControlInput1'
+              placeholder='name@example.com'
+            />
+          </div>
+          <div className='form-group'>
+            <label for='exampleFormControlTextarea1'>
+              Deixe seu comentário:
+            </label>
+            <textarea
+              className='form-control'
+              id='exampleFormControlTextarea1'
+              rows='3'
+            ></textarea>
+          </div>
+          <div className='mt-3'>
+            <button type='submit' className='postarComentario btn btn-success'>
+              Postar
+            </button>
+            <button type='button' className='limparComentario btn btn-light'>
+              Limpar Comentário
+            </button>
+          </div>
+        </form>
+        <hr />
+        <h4>Mural de Mensagens: </h4>
       </div>
     </>
   );
--- projeto-front-end-metta/src/components/Footer/Footer.jsx
+++ projeto-front-end-metta/src/components/Footer/Footer.jsx
@@ -3,12 +3,10 @@ import '../Footer/style.css';
 function Footer() {
   return (
     <>
-      <footer className='text-center text-lg-start bg-light text-muted'>
-        <div className='text-center p-4 d-flex  justify-content-center align-items-center'>
+      <footer className='text-center text-lg-start  '>
+        <div className='design-footer text-center p-4 d-flex  justify-content-center align-items-center text-muted'>
           © 2021 Copyright -{' '}
-          <p className='text-reset fw-bold footer'>
-            Vittório de Andrade Barella
-          </p>
+          <p className='fw-bold design-footer'>Vittório de Andrade Barella</p>
         </div>
       </footer>
     </>
